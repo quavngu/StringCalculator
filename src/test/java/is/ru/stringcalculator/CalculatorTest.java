27e909cac21edbcbbaf5197b5c697d660d6e3717
Quang Nguyen <Kwa@Quangs-MBP.lan>
2015-10-16 16:53:47 +0000

    added some comments in CalculatorTest.java

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,51 +9,61 @@ public class CalculatorTest {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
+    //1
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	//1
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
 
+	//1
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
+	//1
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    //1
     @Test
     public void testOnlyComma() {
     	assertEquals(0, Calculator.add(","));
     }
 
+    //2
     @Test
     public void testUnknownNumberOfArguments() {
     	assertEquals(10, Calculator.add("0,1,1,1,1,1,1,1,1,2"));
     }
 
+    //3
     @Test
     public void testWithEnter() {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
+    //3
     @Test
     public void testWithOnlyEnter() {
     	assertEquals(6, Calculator.add("1\n2\n3"));
     }
 
+    //4
     @Test
     public void testDelimiters() {
     	assertEquals(3, Calculator.add("//;\n1;2"));
     }
 
+    //5
     @Test
     public void testNegativeException() {
     	try {
@@ -64,6 +74,7 @@ public class CalculatorTest {
     	}
     }
 
+    //6
     @Test
     public void testNumberBiggerThan1000() {
     	assertEquals(2, Calculator.add("1001,2"));
