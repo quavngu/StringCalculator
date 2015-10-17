e3184a6d9f3d934cb8d83c1b70bf720d490fc1cd
Quang Nguyen <Kwa@Quangs-MBP.lan>
2015-10-17 18:40:26 +0000

    Forgot to add a small comment on CalculatorTest.java, fixed now

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -86,6 +86,7 @@ public class CalculatorTest {
     	assertEquals(2, Calculator.add("1001,2"));
     }
 
+    //7
     @Test
     public void testDelimitersOfAnyLength() {
         assertEquals(6, Calculator.add("//[***]\n1***2***3"));
